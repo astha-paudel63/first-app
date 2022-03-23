2eb485f692f7af4e7364aa729b6ab3dd114a4841
astha-paudel63 <53424026+astha-paudel63@users.noreply.github.com>
2022-03-23 13:05:58 +0545

    Add add/edit product feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
+import React, { useState } from "react";
 
-function App() {
+const cars = [
+  {
+    name: 'BMW',
+    price: 400,
+  },
+  {
+    name: 'Bentley',
+    price: 300,
+  },
+  {
+    name: 'Tesla',
+    price: 300,
+  },
+  {
+    name: 'Lamborghini',
+    price: 300,
+  },
+  {
+    name: 'Mercedez',
+    price: 300,
+  },
+  {
+    name: 'Porsche',
+    price: 300,
+  },
+  {
+    name: 'Buggatti',
+    price: 300,
+  },
+  {
+    name: 'Ferrari',
+    price: 300,
+  },
+];
+
+function App({ name, location, coOrdinates: {latitude, longitude}, age, primes }) {
+  
+  console.log(name, location, 'ccheck props');
+  // const { name, location, coOrdinates: {latitude, longitude}, age, primes } = props;
+ const [productName, setProductName] =useState('car');
+ const [pricep,productp] = useState(0);
+ const [products,setproducts] =useState([]);
+ const handleAddproduct =(e) => 
+ {setproducts([
+   ...products,
+    {id: Date(),name: productName ,price: pricep}]);
+  setProductName('');
+  productp(0);
+  };
+    const handelRemoveProduct =(id) => setproducts(products.filter((p) =>p.id !== id))
+    const handelLetEditProduct = (product) =>{
+      setProductName(product.name);
+      productp(product.price);
+    };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>{name}</h1>
+      <h1>{location}</h1>
+      <h1>{latitude}' North</h1>
+      <h1>{longitude}'East</h1>
+      <h1>{age} years</h1>
+      <h2>Primes: {primes}  {primes.length} primes</h2>
+      <Counter />
+      <h1>Cars</h1>
+      <ol>
+      {products.map(car => (
+        <li key ={car.id}>
+          <span>{car.name}</span>
+          <span>{car.price}</span>
+          <button onClick={(e) =>handelRemoveProduct(car.id)}>X</button>
+          <button onClick={(f) =>handelLetEditProduct(car)}>Edit</button>
+          </li>
+      ))}
+      </ol>
+      <input value={(productName)}
+      onChange={e => setProductName(e.target.value)}/>
+      <input  type="number"
+      value ={pricep}
+      onChange={f => productp(f.target.value)}
+      />
+      <button  onClick={handleAddproduct}>
+      add
+      </button>
+     </div>
+   
   );
 }
 
